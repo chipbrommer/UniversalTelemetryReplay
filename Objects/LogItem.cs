d8bf740475dab23e9a8d7e62c03ce88b8b532c0b
Chip Brommer <chip.brommer@aseifl.com>
2024-02-09 16:43:36 -0600

    More replay related work

--- Objects/LogItem.cs
+++ Objects/LogItem.cs
@@ -1,14 +1,59 @@
-﻿namespace UniversalTelemetryReplay.Objects
+﻿using System.ComponentModel;
+
+namespace UniversalTelemetryReplay.Objects
 {
-    public class LogItem
+    public class LogItem : INotifyPropertyChanged
     {
         public static int NEXT_ID = 0;
 
-        public int Id { get; set; }
-        public string Configuration { get; set; }
-        public string Status { get; set; }
-        public string FilePath { get; set; }
-        public string IpAddress { get; set; }
-        public int Port { get; set; }
+        private int _id;
+        private string _configuration;
+        private string _status;
+        private string _filePath;
+        private string _ipAddress;
+        private int _port;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; OnPropertyChanged(nameof(Id)); }
+        }
+
+        public string Configuration
+        {
+            get { return _configuration; }
+            set { _configuration = value; OnPropertyChanged(nameof(Configuration)); }
+        }
+
+        public string Status
+        {
+            get { return _status; }
+            set { _status = value; OnPropertyChanged(nameof(Status)); }
+        }
+
+        public string FilePath
+        {
+            get { return _filePath; }
+            set { _filePath = value; OnPropertyChanged(nameof(FilePath)); }
+        }
+
+        public string IpAddress
+        {
+            get { return _ipAddress; }
+            set { _ipAddress = value; OnPropertyChanged(nameof(IpAddress)); }
+        }
+
+        public int Port
+        {
+            get { return _port; }
+            set { _port = value; OnPropertyChanged(nameof(Port)); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
--- Pages/Replay.xaml.cs
+++ Pages/Replay.xaml.cs
@@ -46,7 +46,7 @@ namespace UniversalTelemetryReplay.Pages
             UpdateAddButton();
         }
 
-        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        private void PortTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
             // Check if the input is numeric
             if (!int.TryParse(e.Text, out _))
@@ -57,9 +57,9 @@ namespace UniversalTelemetryReplay.Pages
         }
 
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        private void PortTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            TextBox textBox = (TextBox)sender;
+            TextBox textBox = sender as TextBox;
 
             // Limit the length of the input to 5 numbers
             if (textBox.Text.Length > 5)
@@ -68,6 +68,26 @@ namespace UniversalTelemetryReplay.Pages
                 // Set the caret position to the end
                 textBox.CaretIndex = 5;
             }
+
+            // Find the parent of the TextBox
+            StackPanel parent = textBox.Parent as StackPanel;
+            if (parent != null)
+            {
+                // Find the "Update" button by its name
+                Button updateButton = parent.FindName("UpdateButton") as Button;
+
+                // Enable/disable the "Update" button based on the Port TextBox's text
+                // hide it if the port is empty to prevent the update. 
+                if (!string.IsNullOrEmpty(textBox.Text))
+                {
+                    updateButton.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    updateButton.Visibility = Visibility.Hidden;
+                }
+            }
+
         }
 
         private void UpdateAddButton()
@@ -76,7 +96,7 @@ namespace UniversalTelemetryReplay.Pages
             if (logItems.Count >= MainWindow.settings.MaxReplays)
             {
                 AddLogItemButton.IsEnabled = false;
-                AddLogItemButton.Content = "Disabled - Max Items Reached";
+                AddLogItemButton.Content = "Max Replay Items Reached";
                 TotalItemsTextBlock.Foreground = (Brush)Application.Current.Resources["PrimaryRedColor"];
             }
             else
@@ -97,7 +117,7 @@ namespace UniversalTelemetryReplay.Pages
             {
                 // Create OpenFileDialog
                 OpenFileDialog openFileDialog = new OpenFileDialog();
-                openFileDialog.Filter = "Binary Files (*.bin)|*.bin|Digital Files (*.dig)|*.dig";
+                openFileDialog.Filter = "Digital Files (*.dig)|*.dig|Binary Files (*.bin)|*.bin";
                 openFileDialog.FilterIndex = 1;
                 openFileDialog.Multiselect = false;
 
@@ -114,9 +134,28 @@ namespace UniversalTelemetryReplay.Pages
             }
         }
 
-        private void LogParse_Click(object sender, RoutedEventArgs e)
+        private void LogUpdate_Click(object sender, RoutedEventArgs e)
         {
+            // Get the button that was clicked
+            Button button = sender as Button;
 
+            // Get the data context of the button, which is the item in the list
+            if (button.DataContext is LogItem logItem)
+            {
+                // Find the index of the logItem in the ObservableCollection and update it
+                int index = logItems.IndexOf(logItem);
+                logItems[index].Port = logItem.Port;
+                logItems[index].IpAddress = logItem.IpAddress;
+            }
+
+            // Find the parent of the Button
+            StackPanel parent = button.Parent as StackPanel;
+            if (parent != null)
+            {
+                // Find the "Update" button by its name and hide it
+                Button updateButton = parent.FindName("UpdateButton") as Button;
+                updateButton.Visibility = Visibility.Hidden;
+            }
         }
 
         private void LogRemove_Click(object sender, RoutedEventArgs e)
