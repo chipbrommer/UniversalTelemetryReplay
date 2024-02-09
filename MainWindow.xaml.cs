1554d1e8f703bd7477a9bca06576ee7295c3804f
Chip Brommer <chipbrommer@gmail.com>
2024-02-08 19:16:33 -0600

    start on themes and layout

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,6 @@
-﻿using System.Text;
+﻿using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using UniversalTelemetryReplay.Objects;
 
 namespace UniversalTelemetryReplay
 {
@@ -16,9 +9,38 @@ namespace UniversalTelemetryReplay
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string programDataPath = string.Empty;
+        private readonly string companyFolder = "InnovativeConcepts";
+        private readonly string applicationFolder = "UniversalTelemetryReplay";
+        private readonly string settingsFileName = @"\settings.json";
+        internal static SettingsFile<Settings>? settingsFile;
+        internal static Settings? settings;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            programDataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), companyFolder, applicationFolder);
+
+            // Ensure the ProgramData folder exists, create it if it doesn't
+            if (!Directory.Exists(programDataPath))
+            {
+                Directory.CreateDirectory(programDataPath);
+            }
+
+            // Handle settings file
+            string settingsFilePath = programDataPath + settingsFileName;
+            settingsFile = new SettingsFile<Settings>(settingsFilePath);
+
+            // Attempt to load settings
+            if (!settingsFile.Load() || settingsFile.data == null)
+            {
+                settingsFile.data = new();
+            }
+            settings = settingsFile.data;
+
+            // Update things based on settings.
+            ThemeController.SetTheme(settings.theme);
         }
     }
 }
